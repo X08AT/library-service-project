b63d5005ac3fcde2dd040524b253ab12748b88f4
Artem Turchyn <Artem.at131@gmail.com>
2026-08-20 14:39:04 +0300

    add url for create endpoint

--- borrowings/urls.py
+++ borrowings/urls.py
@@ -1,10 +1,15 @@
 from django.urls import path
 
-from borrowings.views import BorrowingListView, BorrowingRetrieveView
+from borrowings.views import (
+    BorrowingListView,
+    BorrowingRetrieveView,
+    BorrowingCreateView,
+)
 
 app_name = "borrowings"
 
 urlpatterns = [
     path("borrowings/", BorrowingListView.as_view(), name="borrowings"),
     path("borrowings/<int:pk>/", BorrowingRetrieveView.as_view(), name="borrowing"),
+    path("borrowings/create/", BorrowingCreateView.as_view(), name="create_borrowing"),
 ]
