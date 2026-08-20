7745b408ac13e9ff6f7af0ee0cc2771045a0a90d
Artem Turchyn <Artem.at131@gmail.com>
2026-08-20 13:44:57 +0300

    Add view for borrowings list

--- borrowings/views.py
+++ borrowings/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from rest_framework import generics
 
-# Create your views here.
+from borrowings.models import Borrowing
+from borrowings.serializers import BorrowingSerializer
+
+
+class BorrowingListView(generics.ListAPIView):
+    queryset = Borrowing.objects.all()
+    serializer_class = BorrowingSerializer
