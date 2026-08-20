e7b1dfdc4cd0cce39f0901b09e33b5a61b709915
Artem Turchyn <Artem.at131@gmail.com>
2026-08-20 15:51:46 +0300

    Use Flake8 and Black

--- borrowings/urls.py
+++ borrowings/urls.py
@@ -3,7 +3,8 @@ from django.urls import path
 from borrowings.views import (
     BorrowingListView,
     BorrowingRetrieveView,
-    BorrowingCreateView, BorrowingReturnView,
+    BorrowingCreateView,
+    BorrowingReturnView,
 )
 
 app_name = "borrowings"
@@ -12,5 +13,9 @@ urlpatterns = [
     path("borrowings/", BorrowingListView.as_view(), name="borrowings"),
     path("borrowings/<int:pk>/", BorrowingRetrieveView.as_view(), name="borrowing"),
     path("borrowings/create/", BorrowingCreateView.as_view(), name="create_borrowing"),
-    path("borrowings/<int:pk>/return/", BorrowingReturnView.as_view(), name="return_borrowing"),
+    path(
+        "borrowings/<int:pk>/return/",
+        BorrowingReturnView.as_view(),
+        name="return_borrowing",
+    ),
 ]
