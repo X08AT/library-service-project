4cbef4a00536886db993aaa0c0e2dc8f07b811d1
Artem Turchyn <Artem.at131@gmail.com>
2026-08-20 15:49:45 +0300

    Add borrowing return view and url for it

--- borrowings/urls.py
+++ borrowings/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from borrowings.views import (
     BorrowingListView,
     BorrowingRetrieveView,
-    BorrowingCreateView,
+    BorrowingCreateView, BorrowingReturnView,
 )
 
 app_name = "borrowings"
@@ -12,4 +12,5 @@ urlpatterns = [
     path("borrowings/", BorrowingListView.as_view(), name="borrowings"),
     path("borrowings/<int:pk>/", BorrowingRetrieveView.as_view(), name="borrowing"),
     path("borrowings/create/", BorrowingCreateView.as_view(), name="create_borrowing"),
+    path("borrowings/<int:pk>/return/", BorrowingReturnView.as_view(), name="return_borrowing"),
 ]
