1be801e6f54ae897f1e516f9db7628f8ec4d7bf1
Artem Turchyn <Artem.at131@gmail.com>
2026-08-20 13:58:14 +0300

    Add view for borrowing retrieve endpoint

--- borrowings/views.py
+++ borrowings/views.py
@@ -1,9 +1,16 @@
 from rest_framework import generics
 
 from borrowings.models import Borrowing
-from borrowings.serializers import BorrowingSerializer
+from borrowings.serializers import BorrowingSerializer, BorrowingDetailSerializer
 
 
 class BorrowingListView(generics.ListAPIView):
     queryset = Borrowing.objects.all()
     serializer_class = BorrowingSerializer
+
+
+class BorrowingRetrieveView(generics.RetrieveAPIView):
+    serializer_class = BorrowingDetailSerializer
+
+    def get_object(self):
+        return Borrowing.objects.get(pk=self.kwargs["pk"])
