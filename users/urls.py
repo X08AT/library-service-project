33095bf65a6f690ddb9ddc764b95942b32a86384
Artem Turchyn <Artem.at131@gmail.com>
2026-08-21 20:11:25 +0300

    Add documentation for Users API

--- users/urls.py
+++ users/urls.py
@@ -1,16 +1,20 @@
 from django.urls import path
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
-)
 
-from users.views import UserRegistrationView, UserManageView
+
+from users.views import (
+    UserRegistrationView,
+    UserManageView,
+    CustomTokenObtainPairView,
+    CustomTokenRefreshView,
+)
 
 app_name = "users"
 
 urlpatterns = [
-    path("users/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
-    path("users/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path("users/token/", CustomTokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path(
+        "users/token/refresh/", CustomTokenRefreshView.as_view(), name="token_refresh"
+    ),
     path("users/", UserRegistrationView.as_view(), name="create"),
     path("users/me/", UserManageView.as_view(), name="manage"),
 ]
--- users/views.py
+++ users/views.py
@@ -1,8 +1,16 @@
+from drf_spectacular.utils import extend_schema
 from rest_framework import generics, permissions
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 from users.serializers import UserSerializer
 
 
+@extend_schema(
+    summary="User registration",
+    description="Create a new user.",
+    request=UserSerializer,
+    responses=UserSerializer,
+)
 class UserRegistrationView(generics.CreateAPIView):
     serializer_class = UserSerializer
 
@@ -13,3 +21,45 @@ class UserManageView(generics.RetrieveUpdateAPIView):
 
     def get_object(self):
         return self.request.user
+
+    @extend_schema(
+        summary="Retrieve User",
+        description="Retrieve user data.",
+        responses=UserSerializer,
+    )
+    def get(self, request, *args, **kwargs):
+        return super().get(request, *args, **kwargs)
+
+    @extend_schema(
+        summary="Update User",
+        description="Update user data.",
+        request=UserSerializer,
+        responses=UserSerializer,
+    )
+    def put(self, request, *args, **kwargs):
+        return super().put(request, *args, **kwargs)
+
+    @extend_schema(
+        summary="Partial Update User",
+        description="Partially update user data.",
+        request=UserSerializer,
+        responses=UserSerializer,
+    )
+    def patch(self, request, *args, **kwargs):
+        return super().patch(request, *args, **kwargs)
+
+
+@extend_schema(
+    summary="Obtain JWT Token",
+    description="Obtain an access and refresh JWT token pair using user credentials.",
+)
+class CustomTokenObtainPairView(TokenObtainPairView):
+    pass
+
+
+@extend_schema(
+    summary="Refresh JWT Token",
+    description="Obtain a new access token using a valid refresh token.",
+)
+class CustomTokenRefreshView(TokenRefreshView):
+    pass
