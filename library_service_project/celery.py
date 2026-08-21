45c9afc15e1a65b0bdec2f7a051d398717c80fc3
Artem Turchyn <Artem.at131@gmail.com>
2026-08-21 18:35:06 +0300

    Add celery

---
+++ library_service_project/celery.py
@@ -0,0 +1,19 @@
+import os
+
+from celery import Celery
+from celery.schedules import crontab
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "library_service_project.settings")
+
+app = Celery("library_service_project")
+
+app.config_from_object("django.conf:settings", namespace="CELERY")
+
+app.autodiscover_tasks()
+
+app.conf.beat_schedule = {
+    "check-overdue-borrowings-daily": {
+        "task": "borrowings.tasks.check_overdue_borrowings",
+        "schedule": crontab(hour=8, minute=00),
+    },
+}
